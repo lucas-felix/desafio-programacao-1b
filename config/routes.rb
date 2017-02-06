051510159ab53d97d7aad3b35ab13b73ccbc11e5
Lucas Felix <lfelixsampaio@gmail.com>
2017-02-06 09:17:47 -0300

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'purchases#new'
 
-  resources :purchases do
+  resources :purchases, only: [:new] do
     collection { post :import }
   end
   get 'purchases/recent/:quantity' => 'purchases#recent', as: 'recent'
